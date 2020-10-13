fe45bb9c8934ea6f7a1d2bfe66e7108ff57f2d0b
Junior Selister <junior.selister@gmail.com>
2020-10-13 19:20:24 -0300

    Entity User e Rest Ok

---
+++ src/main/java/com/juniorselister/workshopmongo/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.juniorselister.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juniorselister.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	//@GetMapping -> Alternative
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("01", "Maria Brown", "maria@gmail.com");
+		User alex = new User("02", "Alex Green", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	
+}
