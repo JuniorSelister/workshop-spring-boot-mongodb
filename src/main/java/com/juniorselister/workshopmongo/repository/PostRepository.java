080849ad80bf626174b87fdd7252702c5da7b7e1
Junior Selister <junior.selister@gmail.com>
2020-11-10 18:50:09 -0300

    Criando Entity Post com user aninhado

---
+++ src/main/java/com/juniorselister/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.juniorselister.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juniorselister.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+	
+	
+}
