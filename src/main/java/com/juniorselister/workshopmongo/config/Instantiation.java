47fa77842f7e1d8ca0233503aaffac96397290c0
Junior Selister <junior.selister@gmail.com>
2020-11-10 19:46:39 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/juniorselister/workshopmongo/config/Instantiation.java
+++ src/main/java/com/juniorselister/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.juniorselister.workshopmongo.domain.Post;
 import com.juniorselister.workshopmongo.domain.User;
 import com.juniorselister.workshopmongo.dto.AuthorDTO;
+import com.juniorselister.workshopmongo.dto.CommentDTO;
 import com.juniorselister.workshopmongo.repository.PostRepository;
 import com.juniorselister.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feli hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
